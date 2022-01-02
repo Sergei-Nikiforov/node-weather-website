a686216f77b3c9deed45a0b81ae3652fc902557c
Sergei-Nikiforov <pclp@inbox.ru>
2022-01-02 15:28:59 +0300

    Humidity correction

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree out. It feels like a ' + body.current.feelslike + ' degrees out. Humidity is ' + body.current.humidity + '%.')
+            callback(undefined, 
+                body.current.weather_descriptions[0] + '. It is currently ' + 
+                body.current.temperature + ' degree out. It feels like a ' + 
+                body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
         }
     });
 }
